dfd5167e1c97b6b289e62a2034ab0b3670a54b60
bulk <harryhamlin@gmail.com>
2023-02-26 09:06:27 -0800

    added handlebars for homepage with navbar and other html items

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const { Content, Comment, User } = require(`../models`)
 
 // TODO: Add a comment describing the functionality of the withAuth middleware
 router.get('/', async (req, res) => {
   try {
+    const allContent = await Content.findAll();
     res.render('homepage');
+    return res.status(200).json(allContent)
   } catch (err) {
     res.status(500).json(err);
   }
