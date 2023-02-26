717b0ae7bf57011893e26f08b6fc2b356b20635b
bulk <harryhamlin@gmail.com>
2023-02-26 09:08:25 -0800

    fixed a few typos

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,9 +4,9 @@ const { Content, Comment, User } = require(`../models`)
 // TODO: Add a comment describing the functionality of the withAuth middleware
 router.get('/', async (req, res) => {
   try {
-    const allContent = await Content.findAll();
+    // const allContent = await Content.findAll();
     res.render('homepage');
-    return res.status(200).json(allContent)
+    // return res.status(200).json(allContent)
   } catch (err) {
     res.status(500).json(err);
   }
