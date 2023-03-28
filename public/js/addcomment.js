2eea63017c26bcfb4af7344e04815fc0820a9656
bulk <harryhamlin@gmail.com>
2023-03-27 18:03:21 -0700

    heroku test

--- public/js/addcomment.js
+++ public/js/addcomment.js
@@ -1,20 +1,21 @@
-const addCommentButton = document.querySelector('#addComment')
+// const addCommentButton = document.querySelector('#addComment')
 
-const commentFormHandler = async (event) => {
-    event.preventDefault();
-    console.log('click')
-    const comment_body = document.querySelector('#comment').value.trim();
-    const path = window.location.pathname.split('/');
-    const length = path.length
-    const content_id = path[(length - 1)]
-    if (comment_body) {
-        await fetch('/api/comment/', {
-            method: 'POST',
-            body: JSON.stringify({ comment_body, content_id }),
-            headers: { 'Content-Type': 'application/json' },
-        })
-        document.location.replace(`/content/${content_id}`)
-    }
-};
+// const commentFormHandler = async (event) => {
+//     event.preventDefault();
+//     console.log('click')
+//     const comment_body = document.querySelector('#comment').value.trim();
+//     const path = window.location.pathname.split('/');
+//     const length = path.length
+//     const content_id = path[(length - 1)]
+//     if (comment_body) {
+//         await fetch('/api/comment/', {
+//             method: 'POST',
+//             body: JSON.stringify({ comment_body, content_id }),
+//             headers: { 'Content-Type': 'application/json' },
+//         })
+//         document.location.replace(`/content/${content_id}`)
+//     }
+// };
 
-addCommentButton.addEventListener('click', commentFormHandler)
+// addCommentButton.addEventListener('click', commentFormHandler)
+ console.log('click')
